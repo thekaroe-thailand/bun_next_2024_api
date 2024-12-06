3b66d3b5860824dd7370d80d47d59919b8d66d3a
thekaroe-thailand <thekaroe@gmail.com>
2024-12-06 22:34:12 +0700

    workshop

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Elysia } from "elysia";
 import { jwt } from "@elysiajs/jwt";
 import { cors } from "@elysiajs/cors";
 import { UserController } from "./controllers/UserController";
+import { DeviceController } from "./controllers/DeviceController";
 
 const app = new Elysia()
   .use(cors())
@@ -12,6 +13,18 @@ const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .post("/api/user/signin", UserController.signIn)
   .put("/api/user/update", UserController.update)
+
+  //
+  // device
+  //
+  .post("/api/device/create", DeviceController.create)
+  .get("/api/device/list", DeviceController.list)
+  .put("/api/device/update/:id", DeviceController.update)
+  .delete("/api/device/remove/:id", DeviceController.remove)
+
+  // 
+  // listen
+  //
   .listen(3001);
 
 console.log(
